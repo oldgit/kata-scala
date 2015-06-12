f435c693a34df0b5df75a89d7a86b4003f3465b5
oldgit <jon.harvey.uk@gmail.com>
2015-06-12 18:36:38 +0100

    converted to use scalatest which is simpler

--- build.sbt
+++ build.sbt
@@ -1,14 +1,9 @@
 name := """kata-scala"""
 
-version := "0.1.0"
+version := "0.1-SNAPSHOT"
 
 scalaVersion := "2.11.6"
 
-resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
-
-scalacOptions in Test ++= Seq("-Yrangepos")
-
-// Specs2
 libraryDependencies ++= Seq(
-  "org.specs2" %% "specs2-core" % "3.4" % "test",
-  "org.specs2" %% "specs2-html" % "3.4" % "test")
+  "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test",
+  "org.pegdown" % "pegdown" % "1.5.0" % "test")
---
+++ src/test/scala/kata/roman/RomanNumeralsConverterSpec.scala
@@ -0,0 +1,95 @@
+package kata.roman
+
+import org.scalatest._
+
+/**
+ * FlatSpec using scalatest
+ */
+class RomanNumeralsConverterSpec extends FlatSpec with Matchers {
+markup { """
+## RomanNumeralsConverter
+
+The RomanNumeralsConverter has two operations:
+
+* __toRomanNumerals__ to convert a number, e.g. 33 to RomanNumerals, e.g. XXXIII
+    * An _IllegalArgumentException_ is thrown if the number is not in the range 1-3000
+
+* __fromRomanNumerals__ to convert a romanNumeral, e.g. XXXIII to a number, e.g. 33
+    * An _IllegalArgumentException_ is thrown if the roman numerals:
+        * is empty or null
+        * contains illegal characters, .i.e. not I, V, X, L, C, D, M
+        * the order of characters is incorrect
+
+"""}
+
+  /**
+   * converter implementation to test
+   */
+  private val converter: RomanNumeralsConverter = new RomanNumeralsConverterImpl
+
+  /**
+   * valid numbers are in the range 1-3000,
+   * some example invalid numbers
+   */
+  private val invalidNumbers = Set(-1, 0, 3001)
+
+  /**
+   * map of example valid integers to roman numerals
+   */
+  private val validNumberRomanNumerals = Map(
+    1745 -> "MDCCXLV", 33 -> "XXXIII", 45 -> "XLV",
+    1999 -> "MCMXCIX", 3000 -> "MMM", 1888 -> "MDCCCLXXXVIII")
+
+  "The RomanNumeralsConverter.toRomanNumerals" should
+  "throw IllegalArgumentException for numbers not in the range 1-3000: "+invalidNumbers in {
+      for (invalid <- invalidNumbers)
+        an [IllegalArgumentException] should be thrownBy {
+          converter.toRomanNumerals(invalid)
+        }
+  }
+
+  it should "convert some valid numbers: "+validNumberRomanNumerals.keys in {
+    for ((k, v) <- validNumberRomanNumerals) {
+      converter.toRomanNumerals(k) shouldBe v
+    }
+  }
+
+  val nullEmptyMessage = "requirement failed: romanNumerals String is null or empty"
+
+  "The RomanNumeralsConverter.fromRomanNumerals" should
+  s"throw IllegalArgumentException '$nullEmptyMessage' for null" in {
+    the [IllegalArgumentException] thrownBy {
+      converter.fromRomanNumerals(null)
+    } should have message nullEmptyMessage
+  }
+
+  it should s"throw IllegalArgumentException '$nullEmptyMessage' for empty String" in {
+    the [IllegalArgumentException] thrownBy {
+      converter.fromRomanNumerals("")
+    } should have message nullEmptyMessage
+  }
+
+  val invalidRomanNumerals = Set("A", "Z", "IIII", "VXLV", "LXC", "XDCC", "CCMM")
+
+  it should "throw IllegalArgumentException for invalid roman numerals: "+invalidRomanNumerals in {
+    for (romanNumeral <- invalidRomanNumerals) {
+      the [IllegalArgumentException] thrownBy {
+        converter.fromRomanNumerals(romanNumeral)
+      } should have message s"requirement failed: $romanNumeral contains illegal characters or order"
+    }
+  }
+
+  it should "convert some valid roman numerals: "+validNumberRomanNumerals.values.toSet in {
+    for ((k, v) <- validNumberRomanNumerals) {
+      converter.fromRomanNumerals(v) shouldBe k
+    }
+  }
+
+  "The RomanNumeralsConverter.toRomanNumerals & then fromRomanNumerals" should
+  "result in the original number for numbers 1-3000" in {
+    for (number <- 1 to 3000) {
+      number shouldBe converter.fromRomanNumerals(converter.toRomanNumerals(number))
+    }
+  }
+
+}
